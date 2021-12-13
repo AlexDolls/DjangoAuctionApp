c9270a8a5bd8c066720592d769283f9efe5acfc2
AlexDolls <alexdolls4611@gmail.com>
2021-12-14 00:47:48 +0200

    Replaced url routing for websocket consumer from routing.py to asgi.py

--- auctsite/asgi.py
+++ auctsite/asgi.py
@@ -3,16 +3,20 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import market.routing
+from django.conf.urls import url
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auctsite.settings')
 
+from market.consumers import ListingConsumer, ChatConsumer
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
+    
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-                market.routing.websocket_urlpatterns
-            )
+        URLRouter([
+                    url((r"^ws/market/inbox/$"),ChatConsumer.as_asgi()),
+                    url((r"^ws/market/(?P<listing_id>\w+)/$"),ListingConsumer.as_asgi()),
+                    ])
         ),
     # Just HTTP for now. (We can add other protocols later.)
 })
