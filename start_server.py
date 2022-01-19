7f02b4cc227af175973cbdee7f93c75bf0189a25
AlexDolls <alexdolls4611@gmail.com>
2022-01-19 18:25:55 +0200

    Changed start-up script for launch listing for tasks and then celery worker

--- start_server.py
+++ start_server.py
@@ -1,7 +1,6 @@
 import subprocess
 
-process2 = subprocess.Popen(["python3", "manage.py", "runserver"])
-process3 = subprocess.Popen(["python3", "manage.py", "listing_date_checker"])
+process2 = subprocess.Popen(["python3", "manage.py", "listing_date_checker"])
+process3 = subprocess.Popen(["celery", "-A", "auctsite", "worker", "-l", "INFO", "--logfile=/dev-app/logs/celery.log"])
 process2.wait()
 process3.wait()
-
