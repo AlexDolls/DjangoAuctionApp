ec850f9fb2aecb5547f9db38c9f64559f0709f6f
AlexDolls <alexdolls4611@gmail.com>
2022-01-19 18:14:41 +0200

    Celery configuration settings added

--- auctsite/settings.py
+++ auctsite/settings.py
@@ -159,6 +159,10 @@ else:
 
 CHANNEL_LAYERS = ch_layer_temp
 
+# Celery configs
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
+
 #Main url for manage media
 MEDIA_URL = '/media/'
 
