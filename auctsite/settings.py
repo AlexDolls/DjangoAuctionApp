de770bf3706bad5c8efeb5dc5746bedfd8a73b05
AlexDolls <alexdolls4611@gmail.com>
2022-01-24 22:50:33 +0200

    Major reworking of frontend and design features
    Design was reworked on pages: usercabinet.html, inbox.html
    Minor design/froend fixes: detail.html
    Added JS validation: editListing.html, createListing.html
    Added site's logo and favicon

--- auctsite/settings.py
+++ auctsite/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_auctsite")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
--- auctsite/urls.py
+++ auctsite/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -25,3 +27,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("img/favicon.ico")),
+    ),
