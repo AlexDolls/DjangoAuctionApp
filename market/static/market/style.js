9e6a58a41e2d809225c093ef912141116c3d29b0
Sammantha P. Santos <sammanthaps@proton.me>
2023-01-12 20:50:10 -0700

    Login, SignUp

---
+++ market/static/market/style.js
@@ -0,0 +1,6 @@
+const link = document.createElement("link");
+link.setAttribute("rel", "stylesheet");
+link.type = "text/css";
+link.href = "static /theme/css/app-creative.min.css";
+link.id = "light-style";
+document.head.append(link);
