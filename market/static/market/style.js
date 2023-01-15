685715cf74dad2937dc2409d2f06be8f04533cd9
Sammantha P. Santos <sammanthaps@proton.me>
2023-01-14 19:55:32 -0700

    DAA v.1.1.1

--- market/static/market/style.js
+++
@@ -1,6 +0,0 @@
-const link = document.createElement("link");
-link.setAttribute("rel", "stylesheet");
-link.type = "text/css";
-link.href = "static /theme/css/app-creative.min.css";
-link.id = "light-style";
-document.head.append(link);
